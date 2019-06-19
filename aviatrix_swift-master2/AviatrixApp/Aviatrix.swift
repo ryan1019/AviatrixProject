e7a03700a4b7160db839973931f65c885813c1c0
Apple <apple@Apples-MacBook-Pro-86.local>
2019-06-19 14:43:22 -0400

    Complete iteration one

---
+++ aviatrix_swift-master2/AviatrixApp/Aviatrix.swift
@@ -0,0 +1,46 @@
+//
+//  Aviatrix.swift
+//  AviatrixApp
+//
+//  Created by Amy Holt on 6/10/18.
+//  Copyright © 2018 Amy Holt. All rights reserved.
+//
+
+import Foundation
+
+class Aviatrix {
+    var running = false
+    var author : String
+    var data = AviatrixData () 
+    var location = "St. Louis"
+    var distanceTraveled = 0 
+    
+    init(authorName : String) {
+        author = authorName
+        //authorName will be stored in author
+    }
+    
+    func start() ->
+        Bool {
+            running = true
+            return running
+    }
+    
+    func refuel() {
+        
+    }
+    
+    func flyTo(destination : String) {
+        location = destination 
+    }
+    
+    func distanceTo(target : String) -> Int {
+        return data.knownDistances[location]![target]!
+        
+    }
+    
+    func knownDestinations() -> [String] {
+        return ["St. Louis", "Phoenix", "Denver", "SLC"]
+    }
+}
+
